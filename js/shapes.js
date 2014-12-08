e6103591e9d9895076f162550b89e345628a673c
Tin <Ho_tin000@yahoo.com>
2014-12-07 20:31:49 -0800

    Done

--- js/shapes.js
+++ js/shapes.js
@@ -1,15 +1,50 @@
-/*
-    shapes.js
-    This is where your code goes
+"use strict";
 
-    Write the code to create rectangle and circle classes that extend the
-    Shape class defined in shape.js. Then create a couple of other subclasses that
-    render different sorts of shapes using the HTML <canvas> functions.
-    http://www.w3schools.com/tags/ref_canvas.asp
+function Rectangle(left, top, width, height, stylesMap) {
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.stylesMap = stylesMap;
+    this.renderShape = function(canvasCtx) {
+        return canvasCtx.fillRect(this.left, this.top, this.width, this.height);
+    };
+}
 
-    You can use either the classical or the prototypal style to create your subclasses
+Rectangle.prototype = new Shape();
+registerClassicalShape('Rectangle', Rectangle);
 
-    After you've written the code for the sublcasses, call either registerPrototypalShape()
-    or registerClassicalShape() to register your new shapes with the application. See the
-    app.js file for info on these functions.
- */
+function Circle(left, top, width, height, stylesMap) {
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.stylesMap = stylesMap;
+    this.renderShape = function(canvasCtx) {
+        canvasCtx.beginPath();
+        canvasCtx.arc(this.left, this.top, this.height/2, 0, (Math.PI * 2));
+        return canvasCtx.fill();
+    }
+}
+
+Circle.prototype = new Shape();
+registerClassicalShape('Circle', Circle);
+
+function Taco(left, top, width, height, stylesMap) {
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.stylesMap = stylesMap;
+    this.renderShape = function(canvasCtx) {
+        var gradient = canvasCtx.createLinearGradient(0,0, document.getElementById("shapes-canvas").width, 0);
+        gradient.addColorStop("0", "blue");
+        gradient.addColorStop("0.5", "green");
+        gradient.addColorStop("1.0", "red");
+        canvasCtx.fillStyle = gradient;
+        return canvasCtx.fillText("TACO", this.left, this.top);
+    }
+}
+
+Taco.prototype = new Shape();
+registerClassicalShape('Taco', Taco);
